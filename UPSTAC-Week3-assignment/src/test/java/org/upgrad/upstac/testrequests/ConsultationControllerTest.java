3a4e1ce2d24e352c8f7a7cb1499db60fe2ef8c43
Swanand <swanandmehta94@gmail.com>
2021-11-29 15:09:46 +0200

    Updating testcases

--- UPSTAC-Week3-assignment/src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
+++ UPSTAC-Week3-assignment/src/test/java/org/upgrad/upstac/testrequests/ConsultationControllerTest.java
@@ -5,19 +5,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.web.server.ResponseStatusException;
 import org.upgrad.upstac.testrequests.TestRequest;
+import org.upgrad.upstac.testrequests.consultation.Consultation;
 import org.upgrad.upstac.testrequests.consultation.ConsultationController;
 import org.upgrad.upstac.testrequests.consultation.CreateConsultationRequest;
+import org.upgrad.upstac.testrequests.consultation.DoctorSuggestion;
+import org.upgrad.upstac.testrequests.lab.CreateLabResult;
 import org.upgrad.upstac.testrequests.lab.TestStatus;
 import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
+import org.upgrad.upstac.users.User;
+import org.upgrad.upstac.users.models.Gender;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -32,25 +41,76 @@ class ConsultationControllerTest {
     @Autowired
     TestRequestQueryService testRequestQueryService;
 
+    @Autowired
+    TestRequestService testRequestService;
+
+    @Autowired
+    TestRequestUpdateService testRequestUpdateService;
+
+
+    private static User user;
+    private static User tester;
+
+    @BeforeTestClass
+    public static void setUpBeforeClass() throws Exception {
+        user = createUser();
+        tester = createUser();
+    }
+
 
     @Test
     @WithUserDetails(value = "doctor")
     public void calling_assignForConsultation_with_valid_test_request_id_should_update_the_request_status(){
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_COMPLETED);
+        CreateTestRequest createTestRequest = createTestRequest();
 
-        //Implement this method
+        TestRequest testRequest = testRequestService.createTestRequestFrom(user,createTestRequest);
 
-        //Create another object of the TestRequest method and explicitly assign this object for Consultation using assignForConsultation() method
-        // from consultationController class. Pass the request id of testRequest object.
+        testRequestUpdateService.assignForLabTest(testRequest.requestId, tester);
 
-        //Use assertThat() methods to perform the following two comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'DIAGNOSIS_IN_PROCESS'
-        // make use of assertNotNull() method to make sure that the consultation value of second object is not null
-        // use getConsultation() method to get the lab result
+        testRequestUpdateService.updateLabTest(testRequest.requestId, getCreateLabResult(testRequest), tester);
 
+        testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_COMPLETED);
 
+        TestRequest testRequest1 = consultationController.assignForConsultation(testRequest.requestId);
+
+        assertNotNull(testRequest1);
+        assertEquals(testRequest.requestId, testRequest1.requestId);
+        assertEquals(testRequest1.getStatus(), RequestStatus.DIAGNOSIS_IN_PROCESS);
+    }
+
+    private static User createUser() {
+        User user = new User();
+        user.setUserName(UUID.randomUUID().toString());
+        return user;
+    }
+
+    public CreateTestRequest createTestRequest() {
+        Random random = new Random();
+        CreateTestRequest createTestRequest = new CreateTestRequest();
+        createTestRequest.setAddress("some Addres");
+        createTestRequest.setAge(98);
+        createTestRequest.setEmail("someone" + UUID.randomUUID().toString() + "@somedomain.com");
+        createTestRequest.setGender(Gender.MALE);
+        createTestRequest.setName("someuser");
+        createTestRequest.setPhoneNumber(Integer.toString(ThreadLocalRandom.current().nextInt(100000, 999998 + 1)));
+        createTestRequest.setPinCode(716768);
+        return createTestRequest;
+    }
+
+    public CreateLabResult getCreateLabResult(TestRequest testRequest) {
+
+        //Create an object of CreateLabResult and set all the values
+        // Return the object
+        CreateLabResult result = new CreateLabResult();
+        result.setBloodPressure("N/A");
+        result.setHeartBeat("N/A");
+        result.setTemperature("N/A");
+        result.setOxygenLevel("N/A");
+        result.setComments("N/A");
+        result.setResult(TestStatus.NEGATIVE);
+
+        return result;
     }
 
     public TestRequest getTestRequestByStatus(RequestStatus status) {
@@ -63,39 +123,35 @@ class ConsultationControllerTest {
 
         Long InvalidRequestId= -34L;
 
-        //Implement this method
+        ResponseStatusException error = assertThrows(ResponseStatusException.class, () -> {
+            consultationController.assignForConsultation(InvalidRequestId);
+        });
 
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass assignForConsultation() method
-        // of consultationController with InvalidRequestId as Id
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
-
+        assertThat("Invalid Id", error.getMessage().contains("Invalid ID"));
     }
 
     @Test
     @WithUserDetails(value = "doctor")
     public void calling_updateConsultation_with_valid_test_request_id_should_update_the_request_status_and_update_consultation_details(){
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+        CreateTestRequest createTestRequest = createTestRequest();
 
-        //Implement this method
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
+        TestRequest testRequest = testRequestService.createTestRequestFrom(user,createTestRequest);
 
-        //Create another object of the TestRequest method and explicitly update the status of this object
-        // to be 'COMPLETED'. Make use of updateConsultation() method from consultationController class
-        // (Pass the previously created two objects as parameters)
-        // (for the object of TestRequest class, pass its ID using getRequestId())
+        testRequestUpdateService.assignForLabTest(testRequest.requestId, tester);
 
-        //Use assertThat() methods to perform the following three comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'COMPLETED'
-        // 3. the suggestion of both the objects created should be same. Make use of getSuggestion() method to get the results.
+        testRequestUpdateService.updateLabTest(testRequest.requestId, getCreateLabResult(testRequest), tester);
 
+        consultationController.assignForConsultation(testRequest.requestId);
 
+        testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
 
+        TestRequest testRequest1 = consultationController.updateConsultation(testRequest.requestId, getCreateConsultationRequest(testRequest));
+        testRequest = testRequestQueryService.getTestRequestById(testRequest1.requestId).get();
+
+        assertEquals(testRequest.getStatus(), RequestStatus.COMPLETED);
+        assertEquals(testRequest1.requestId, testRequest.requestId);
+        assertEquals(testRequest1.getConsultation().getSuggestion(), testRequest.getConsultation().getSuggestion());
     }
 
 
@@ -103,19 +159,22 @@ class ConsultationControllerTest {
     @WithUserDetails(value = "doctor")
     public void calling_updateConsultation_with_invalid_test_request_id_should_throw_exception(){
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+        CreateTestRequest createTestRequest = createTestRequest();
 
-        //Implement this method
+        TestRequest testRequest = testRequestService.createTestRequestFrom(user,createTestRequest);
 
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
+        testRequestUpdateService.assignForLabTest(testRequest.requestId, tester);
 
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateConsultation() method
-        // of consultationController with a negative long value as Id and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
+        testRequestUpdateService.updateLabTest(testRequest.requestId, getCreateLabResult(testRequest), tester);
 
+        consultationController.assignForConsultation(testRequest.requestId);
 
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
+        ResponseStatusException err = assertThrows(ResponseStatusException.class, () -> {
+            TestRequest testRequest1 = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+            consultationController.updateConsultation(-1L, getCreateConsultationRequest(testRequest1));
+        });
+
+        assertThat("Invalid ID", err.getMessage().contains("Invalid ID"));
 
     }
 
@@ -123,29 +182,37 @@ class ConsultationControllerTest {
     @WithUserDetails(value = "doctor")
     public void calling_updateConsultation_with_invalid_empty_status_should_throw_exception(){
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+        CreateTestRequest createTestRequest = createTestRequest();
 
-        //Implement this method
+        TestRequest testRequest = testRequestService.createTestRequestFrom(user,createTestRequest);
 
-        //Create an object of CreateConsultationRequest and call getCreateConsultationRequest() to create the object. Pass the above created object as the parameter
-        // Set the suggestion of the above created object to null.
+        testRequestUpdateService.assignForLabTest(testRequest.requestId, tester);
 
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateConsultation() method
-        // of consultationController with request Id of the testRequest object and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
+        testRequestUpdateService.updateLabTest(testRequest.requestId, getCreateLabResult(testRequest), tester);
 
+        consultationController.assignForConsultation(testRequest.requestId);
 
+        testRequest = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+
+        ResponseStatusException err = assertThrows(ResponseStatusException.class, () -> {
+            TestRequest testRequest1 = getTestRequestByStatus(RequestStatus.DIAGNOSIS_IN_PROCESS);
+            CreateConsultationRequest request = getCreateConsultationRequest(testRequest1);
+            request.setSuggestion(null);
+            consultationController.updateConsultation(testRequest1.requestId, request);
+        });
     }
 
     public CreateConsultationRequest getCreateConsultationRequest(TestRequest testRequest) {
 
-        //Create an object of CreateLabResult and set all the values
-        // if the lab result test status is Positive, set the doctor suggestion as "HOME_QUARANTINE" and comments accordingly
-        // else if the lab result status is Negative, set the doctor suggestion as "NO_ISSUES" and comments as "Ok"
-        // Return the object
+        CreateConsultationRequest result = new CreateConsultationRequest();
 
+        if (testRequest.getLabResult().getResult() == TestStatus.POSITIVE) {
+            result.setSuggestion(DoctorSuggestion.HOME_QUARANTINE);
+        } else {
+            result.setSuggestion(DoctorSuggestion.NO_ISSUES);
+        }
 
-        return null; // Replace this line with your code
+        return result;
 
     }
 
--- UPSTAC-Week3-assignment/src/test/java/org/upgrad/upstac/testrequests/LabRequestControllerTest.java
+++ UPSTAC-Week3-assignment/src/test/java/org/upgrad/upstac/testrequests/LabRequestControllerTest.java
@@ -5,16 +5,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.upgrad.upstac.exception.AppException;
+import org.upgrad.upstac.exception.UpgradResponseStatusException;
 import org.upgrad.upstac.testrequests.lab.CreateLabResult;
 import org.upgrad.upstac.testrequests.lab.LabRequestController;
 import org.upgrad.upstac.testrequests.lab.TestStatus;
+import org.upgrad.upstac.users.User;
+import org.upgrad.upstac.users.models.Gender;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -26,29 +31,51 @@ class LabRequestControllerTest {
     LabRequestController labRequestController;
 
 
-
-
     @Autowired
     TestRequestQueryService testRequestQueryService;
 
+
+    @Autowired
+    TestRequestService testRequestService;
+
+    private static User user;
+
+    @BeforeTestClass
+    public static void setUpBeforeClass() throws Exception {
+        user = createUser();
+    }
 
     @Test
     @WithUserDetails(value = "tester")
     public void calling_assignForLabTest_with_valid_test_request_id_should_update_the_request_status(){
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.INITIATED);
-        //Implement this method
+        CreateTestRequest createTestRequest = createTestRequest();
+        TestRequest testRequest = testRequestService.createTestRequestFrom(user,createTestRequest);
 
-        //Create another object of the TestRequest method and explicitly assign this object for Lab Test using assignForLabTest() method
-        // from labRequestController class. Pass the request id of testRequest object.
+        TestRequest testRequest1 = labRequestController.assignForLabTest(testRequest.requestId);
 
-        //Use assertThat() methods to perform the following two comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'LAB_TEST_IN_PROGRESS'
-        // make use of assertNotNull() method to make sure that the lab result of second object is not null
-        // use getLabResult() method to get the lab result
+        assertNotNull(testRequest1);
+        assertEquals(testRequest.requestId, testRequest1.requestId);
+        assertEquals(testRequest1.getStatus(), RequestStatus.LAB_TEST_IN_PROGRESS);
+    }
 
+    private static User createUser() {
+        User user = new User();
+        user.setUserName(UUID.randomUUID().toString());
+        return user;
+    }
 
+    public CreateTestRequest createTestRequest() {
+        Random random = new Random();
+        CreateTestRequest createTestRequest = new CreateTestRequest();
+        createTestRequest.setAddress("some Addres");
+        createTestRequest.setAge(98);
+        createTestRequest.setEmail("someone" + UUID.randomUUID().toString() + "@somedomain.com");
+        createTestRequest.setGender(Gender.MALE);
+        createTestRequest.setName("someuser");
+        createTestRequest.setPhoneNumber(Integer.toString(ThreadLocalRandom.current().nextInt(100000, 999998 + 1)));
+        createTestRequest.setPinCode(716768);
+        return createTestRequest;
     }
 
     public TestRequest getTestRequestByStatus(RequestStatus status) {
@@ -61,15 +88,11 @@ class LabRequestControllerTest {
 
         Long InvalidRequestId= -34L;
 
-        //Implement this method
+        AppException thrown = assertThrows(AppException.class, () -> {
+            labRequestController.assignForLabTest(InvalidRequestId);
+        });
 
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass assignForLabTest() method
-        // of labRequestController with InvalidRequestId as Id
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
+        assertThat("Invalid ID", thrown.getMessage().contains("Invalid ID"));
 
     }
 
@@ -77,20 +100,15 @@ class LabRequestControllerTest {
     @WithUserDetails(value = "tester")
     public void calling_updateLabTest_with_valid_test_request_id_should_update_the_request_status_and_update_test_request_details(){
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_IN_PROGRESS);
+        CreateTestRequest createTestRequest = createTestRequest();
+        TestRequest testRequest = testRequestService.createTestRequestFrom(user,createTestRequest);
 
-        //Implement this method
-        //Create an object of CreateLabResult and call getCreateLabResult() to create the object. Pass the above created object as the parameter
+        testRequest = labRequestController.assignForLabTest(testRequest.requestId);
 
-        //Create another object of the TestRequest method and explicitly update the status of this object
-        // to be 'LAB_TEST_IN_PROGRESS'. Make use of updateLabTest() method from labRequestController class (Pass the previously created two objects as parameters)
+        TestRequest testRequest1 = labRequestController.updateLabTest(testRequest.requestId, getCreateLabResult(testRequest));
 
-        //Use assertThat() methods to perform the following three comparisons
-        //  1. the request ids of both the objects created should be same
-        //  2. the status of the second object should be equal to 'LAB_TEST_COMPLETED'
-        // 3. the results of both the objects created should be same. Make use of getLabResult() method to get the results.
-
-
+        assertEquals(testRequest.requestId, testRequest1.requestId);
+        assertEquals(testRequest1.getStatus(), RequestStatus.LAB_TEST_COMPLETED);
 
     }
 
@@ -98,52 +116,40 @@ class LabRequestControllerTest {
     @Test
     @WithUserDetails(value = "tester")
     public void calling_updateLabTest_with_invalid_test_request_id_should_throw_exception(){
+        Long InvalidRequestId= -34L;
 
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_IN_PROGRESS);
+        UpgradResponseStatusException thrown = assertThrows(UpgradResponseStatusException.class, () -> {
+            labRequestController.updateLabTest(InvalidRequestId, getCreateLabResult(null));
+        });
 
-
-        //Implement this method
-
-        //Create an object of CreateLabResult and call getCreateLabResult() to create the object. Pass the above created object as the parameter
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateLabTest() method
-        // of labRequestController with a negative long value as Id and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "Invalid ID"
+        assertThat("Invalid ID", thrown.getMessage().contains("Invalid ID"));
 
     }
 
     @Test
     @WithUserDetails(value = "tester")
     public void calling_updateLabTest_with_invalid_empty_status_should_throw_exception(){
-
-        TestRequest testRequest = getTestRequestByStatus(RequestStatus.LAB_TEST_IN_PROGRESS);
-
-        //Implement this method
-
-        //Create an object of CreateLabResult and call getCreateLabResult() to create the object. Pass the above created object as the parameter
-        // Set the result of the above created object to null.
-
-        // Create an object of ResponseStatusException . Use assertThrows() method and pass updateLabTest() method
-        // of labRequestController with request Id of the testRequest object and the above created object as second parameter
-        //Refer to the TestRequestControllerTest to check how to use assertThrows() method
-
-
-        //Use assertThat() method to perform the following comparison
-        //  the exception message should be contain the string "ConstraintViolationException"
-
+        NullPointerException thrown = assertThrows(NullPointerException.class, () -> {
+            CreateTestRequest createTestRequest = createTestRequest();
+            TestRequest testRequest = testRequestService.createTestRequestFrom(user,createTestRequest);
+            testRequest = labRequestController.assignForLabTest(testRequest.requestId);
+            labRequestController.updateLabTest(testRequest.requestId, null);
+        });
     }
 
     public CreateLabResult getCreateLabResult(TestRequest testRequest) {
 
         //Create an object of CreateLabResult and set all the values
         // Return the object
+        CreateLabResult result = new CreateLabResult();
+        result.setBloodPressure("N/A");
+        result.setHeartBeat("N/A");
+        result.setTemperature("N/A");
+        result.setOxygenLevel("N/A");
+        result.setComments("N/A");
+        result.setResult(TestStatus.NEGATIVE);
 
-
-        return null; // Replace this line with your code
+        return result;
     }
 
 }
